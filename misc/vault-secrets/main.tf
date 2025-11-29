2522755e5c56be606fc754fdf5ee4bc3fcc2e309
kishor <kishorebsc12@gmail.com>
2025-11-29 12:35:48 +0530

    added

---
+++ misc/vault-secrets/main.tf
@@ -0,0 +1,31 @@
+provider "vault" {
+  address = "http://vault.kommanuthala.store:8200"
+  token = var.vault_token
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-b87"
+    key = "vault-secrets/state"
+    region = "us-east-1"
+  }
+}
+
+variable "vault_token" {}
+
+resource "vault_mount" "ssh" {
+  path = "infra"
+  type = "kv"
+  options = { version = "2" }
+  description = "infra-secrets"
+}
+
+resource "vault_generic_secret" "ssh" {
+  path = "${vault_mount.ssh.path}/ssh"
+  data_json = jsonencode(
+    {
+        "username" : "ec2-user",
+        "password" : "DevOps321"
+    }
+  )
+}
